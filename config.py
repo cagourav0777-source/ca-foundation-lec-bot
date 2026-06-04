1ebb1101eb5a11d8ed7082a36bede33dedbe644a
Gourav kumar <cagourav0777@gmail.com>
2026-06-04 15:33:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = ""
-api_hash = ""
-bot_token = ""
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "32415613"
+api_hash = "7addb7dbc2649fff55a8920ea57fee1a"
+bot_token = "8977661576:AAE3Jg5FQuci5gtj9o0x6o09sAKZm2zMmtk"
+auth_users = [7480556976]
+sudo_users = [7480556976]
