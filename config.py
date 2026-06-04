82c1010648533b81557b2c1e036ef8e7f42513b1
Gourav kumar <cagourav0777@gmail.com>
2026-06-04 15:51:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-api_id = "32415613"
-api_hash = "7addb7dbc2649fff55a8920ea57fee1a"
-bot_token = "8977661576:AAE3Jg5FQuci5gtj9o0x6o09sAKZm2zMmtk"
-auth_users = [7480556976]
-sudo_users = [7480556976]
+import os
+
+api_id = int(os.environ.get("API_ID", 1234567))
+api_hash = os.environ.get("API_HASH", "your_api_hash_here")
+bot_token = os.environ.get("BOT_TOKEN", "your_bot_token_here")
+auth_users = [7890781002]  # Apna Telegram ID
+sudo_users = [7890781002]  # Apna Telegram ID
