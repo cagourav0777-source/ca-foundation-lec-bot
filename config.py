2132da90c3881cbda59ad7c61a9fbc0ebbac2305
Gourav kumar <cagourav0777@gmail.com>
2026-06-04 17:22:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ api_hash = os.environ.get("API_HASH", "your_api_hash_here")
 bot_token = os.environ.get("BOT_TOKEN", "your_bot_token_here")
 auth_users = [7480556976]  # Apna Telegram ID
 sudo_users = [7480556976]  # Apna Telegram ID
+
