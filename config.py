ef9db5520d96760129efff949286aa6d3c40947a
Gourav kumar <cagourav0777@gmail.com>
2026-06-04 14:14:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+AUTH_USERS = [7890781002]
 sudo_users = [7890781002]
